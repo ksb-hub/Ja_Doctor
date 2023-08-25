fda57e4ccc6043182ea022dc4a5029d0bb959564
kim <gayun53a@naver.com>
2023-08-25 18:52:39 +0900

    add the Swagger URL

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/statement/", include('statements.urls')),
 
 ]
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Ja_Doctor Project",
+        default_version='v1.1.1',
+        description="Ja_Doctor API 문서",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="gayun53a@korea.ac.kr"),  # 부가정보
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns += [path(r'swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path(r'redoc', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
+                ]
--- statements/views.py
+++ statements/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -8,12 +9,18 @@ from statements.serializers import StatementSerializer, StatementRetrieveSeriali
 
 
 class PostListAPIView(APIView):
+    @swagger_auto_schema(tags=["post_"])
     def get(self, request, statement_id):
         statement = Statement.objects.get(id=statement_id)
         posts = statement.posts.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(tags=["post_"], request_body=PostSerializer, query_serializer=PostSerializer,
+                         responses={
+                             201: 'post 객체 생성 완료',
+                             400: '입력값 유효성 검증 실패',
+                         })
     def post(self, request, statement_id):
         serializer = PostSerializer(data=request.data)
         if serializer.is_valid():
@@ -22,6 +29,11 @@ class PostListAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PostRetrieveAPIView(APIView):
+    @swagger_auto_schema(tags=["post_detail"], request_body=PostSerializer, query_serializer=PostSerializer,
+                         responses={
+                             202: 'post 객체 수정 완료',
+                             400: '입력값 유효성 검증 실패',
+                         })
     def put(self, request, statement_id, post_id):
         post = get_object_or_404(Post, id=post_id)
         serializer = PostSerializer(post, data=request.data)
@@ -32,11 +44,17 @@ class PostRetrieveAPIView(APIView):
 
 
 class StatementListAPIView(APIView):
+    @swagger_auto_schema(tags=["statement_"])
     def get(self, request):
         statements = Statement.objects.all()
         serializer = StatementSerializer(statements, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(tags=["statement_"], request_body=StatementSerializer,
+                         responses={
+                             201: 'statement 객체 생성 완료',
+                             400: '입력값 유효성 검증 실패',
+                         })
     def post(self, request):
         serializer = StatementSerializer(data=request.data)
         if serializer.is_valid():
@@ -46,19 +64,31 @@ class StatementListAPIView(APIView):
 
 
 class StatementRetrieveAPIView(APIView):
+    @swagger_auto_schema(tags=["statement_detail"])
     def get(self, request, statement_id):
         statement = get_object_or_404(Statement, id=statement_id)
         serializer = StatementRetrieveSerializer(statement)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    @swagger_auto_schema(tags=["statement_detail"], request_body=StatementRetrieveSerializer,
+                         query_serializer=StatementRetrieveSerializer,
+                         responses={
+                             202: 'statement 객체 수정 완료',
+                             400: '입력값 유효성 검증 실패',
+                         })
     def put(self, request, statement_id):
         statement = get_object_or_404(Statement, id=statement_id)
         serializer = StatementRetrieveSerializer(statement, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @swagger_auto_schema(tags=["statement_detail"], request_body=StatementRetrieveSerializer,
+                         query_serializer=StatementRetrieveSerializer,
+                         responses={
+                             204: 'statement 객체 삭제 완료',
+                         })
     def delete(self, request, statement_id):
         statement = get_object_or_404(Statement, id=statement_id)
         statement.delete()
